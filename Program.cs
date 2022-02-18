f9df6e285523525599b90e55079415e89cd348db
JOY <joybosu@gmail.com>
2022-02-18 20:02:59 +0530

    Add to git

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace  Add
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter fast number a= :");
+
+            Console.WriteLine("Enter secound number b=:");
+
+
+            Add.AddTwoNum();
+            // Sub.SubTwoNum();
+            int c = a + b;
+        }
+    }
+}
